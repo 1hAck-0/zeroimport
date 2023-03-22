719700041b10b947dfc3bf14db05309e19562287
1hAck <85506920+1hAck-0@users.noreply.github.com>
2023-03-22 17:30:22 +0100

    Update zeroimport.cpp

--- zeroimport.cpp
+++ zeroimport.cpp
@@ -7,9 +7,14 @@ namespace zeroimport
 {
 	bool init()
 	{
+		return init(detail::GetPsLoadedModuleList());
+	}
+
+	bool init(PLIST_ENTRY pModuleEntryList)
+	{
 		using namespace detail;
 
-		PLDR_DATA_TABLE_ENTRY NtoskrnlEntry = GetSystemModuleEntry(L"ntoskrnl.exe");
+		PLDR_DATA_TABLE_ENTRY NtoskrnlEntry = GetSystemModuleEntry(pModuleEntryList, L"ntoskrnl.exe");
 		if (!NtoskrnlEntry || !NtoskrnlEntry->DllBase)
 			return false;
 
@@ -26,16 +31,19 @@ namespace zeroimport
 		return true;
 	}
 
+	bool init(PDRIVER_OBJECT pDriverObject)
+	{
+		auto pModuleEntry = (detail::PLDR_DATA_TABLE_ENTRY)pDriverObject->DriverSection;
+		return init(pModuleEntry->InLoadOrderModuleList.Flink);
+	}
+
 	namespace detail
 	{
 		uintptr_t NtoskrnlBase = 0;
 		uintptr_t NtoskrnlExportDir = 0;
 
-		PLDR_DATA_TABLE_ENTRY GetSystemModuleEntry(const wchar_t* ModuleName)
+		PLIST_ENTRY GetPsLoadedModuleList()
 		{
-			if (!ModuleName || !ModuleName[0])
-				return 0;
-
 			auto RtlInitUnicodeString = [](PUNICODE_STRING DestinationString, PCWSTR SourceString)->void
 			{
 				DestinationString->Buffer = (wchar_t*)SourceString;
@@ -47,6 +55,23 @@ namespace zeroimport
 				DestinationString->Length = StrLen * sizeof(wchar_t);
 				DestinationString->MaximumLength = ++StrLen * sizeof(wchar_t);
 			};
+
+			static PLIST_ENTRY pModuleList = 0;
+			if (!pModuleList)
+			{
+				UNICODE_STRING UnicodeBuf;
+				RtlInitUnicodeString(&UnicodeBuf, L"PsLoadedModuleList");
+
+				pModuleList = (PLIST_ENTRY)MmGetSystemRoutineAddress(&UnicodeBuf);
+			}
+
+			return pModuleList;
+		}
+
+		PLDR_DATA_TABLE_ENTRY GetSystemModuleEntry(PLIST_ENTRY pModuleEntryList, const wchar_t* ModuleName)
+		{
+			if (!ModuleName || !ModuleName[0])
+				return 0;
 
 			auto wcscimp = [](const wchar_t* str1, const wchar_t* str2)->bool
 			{
@@ -65,18 +90,7 @@ namespace zeroimport
 				return true;
 			};
 
-			static PLIST_ENTRY pModuleList = 0;
-			if (!pModuleList)
-			{
-				UNICODE_STRING UnicodeBuf;
-				RtlInitUnicodeString(&UnicodeBuf, L"PsLoadedModuleList");
-
-				pModuleList = (PLIST_ENTRY)MmGetSystemRoutineAddress(&UnicodeBuf);
-				if (!pModuleList)
-					return 0;
-			}
-
-			for (PLIST_ENTRY pEntry = pModuleList; pEntry != pModuleList->Blink; pEntry = pEntry->Flink)
+			for (PLIST_ENTRY pEntry = pModuleEntryList; pEntry != pModuleEntryList->Blink; pEntry = pEntry->Flink)
 			{
 				PLDR_DATA_TABLE_ENTRY ModuleEntry = CONTAINING_RECORD(pEntry, LDR_DATA_TABLE_ENTRY, InLoadOrderModuleList);
 				if (!ModuleEntry->BaseDllName.Buffer)
